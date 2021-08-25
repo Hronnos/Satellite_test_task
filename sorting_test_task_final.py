433cca8460854f687f6b64792023a398b91dcb8a
Hronnos <66788792+Hronnos@users.noreply.github.com>
2021-08-25 19:52:03 +0300

    Update sorting_test_task_final.py
    Added unpacking according to a new condition

--- sorting_test_task_final.py
+++ sorting_test_task_final.py
@@ -1,14 +1,25 @@
 # final_method
-list_of_values = [(1, 'event11'), (5, 'event12'), (15, 'event13'),
-                  (3, 'event21'), (5, 'event22'), (7, 'event23'), (2, 'event31')
+list_of_values = [[(1, 'event11'), (5, 'event12'), (15, 'event13')],
+                  [(3, 'event21'), (5, 'event22'), (7, 'event23')], [(2, 'event31')]
                   ]
-sorted_list = []
-def gen(list_of_values):
-   while len(list_of_values) != 0:
-      minn = list_of_values.pop(list_of_values.index(min(list_of_values)))
-      yield minn
+lv = []
+def flatten(value, depth):
+    if depth and isinstance(value, list):
+        for v in value: yield from flatten(v, depth - 1)
+    else:
+        yield value
+        
+for i in flatten(list_of_values, 2):
+    lv.append(i)
 
-for i in gen(list_of_values):
-   sorted_list.append(i)
+sorted_list = []
+def gen(lv):
+    while len(lv) != 0:
+        minn = lv.pop(lv.index(min(lv)))
+        yield minn
+
+for i in gen(lv):
+    sorted_list.append(i)
 
 print(sorted_list)
+
